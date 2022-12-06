463323dcc03724f06bb5ed06b345383b4396e2ac
julio <julioroja987@gmail.com>
2022-12-05 23:46:38 -0300

    Habia un error en la responsibidad del Hero en altura

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,9 @@ const Hero = () => {
                     />
                 </div>
                 {/* Container de texto sobre imagen */}
-                <div className="absolute inset-0 z-10">
+                <div className="absolute inset-0 z-10 flex flex-col pb-20">
                     {/* Texto sobre video */}
-                    <div className="mb-[183px] h-auto px-8 pt-14 lg:mb-[300px] lg:px-32 lg:pt-28 ">
+                    <div className="mb-auto px-8 pt-14 lg:px-32 lg:pt-28 ">
                         <div>
                             <h1 className="w-64 pb-2 text-[27px] font-bold leading-8 text-white  lg:w-[498px] lg:text-[60px] lg:leading-[72px]">
                                 Una oportunidad para crecer
@@ -27,16 +27,19 @@ const Hero = () => {
                         </div>
                     </div>
                     {/* Botones Venta y Alquiler */}
-                    <h2 className="pb-6 text-center text-base font-bold text-white lg:text-2xl">
-                        ¿Qué estás buscando hoy?
-                    </h2>
-                    <div className="flex justify-center">
-                        <button className="h-[53px] w-[150px] rounded-tl rounded-bl  bg-black text-base text-white lg:h-[50px] lg:w-[211px] lg:text-[18px] lg:font-medium">
-                            Vender
-                        </button>
-                        <button className="h-[53px] w-[150px] rounded-tr rounded-br bg-cafranGray-semiDark text-base text-white lg:h-[50px] lg:w-[211px] lg:text-[18px] lg:font-medium">
-                            Alquilar
-                        </button>
+
+                    <div className="flex flex-col items-center justify-center">
+                        <h2 className="pb-6 text-center text-base font-bold text-white lg:text-2xl">
+                            ¿Qué estás buscando hoy?
+                        </h2>
+                        <div>
+                            <button className="h-[53px] w-[150px] rounded-tl rounded-bl  bg-black text-base text-white lg:h-[50px] lg:w-[211px] lg:text-[18px] lg:font-medium">
+                                Vender
+                            </button>
+                            <button className="h-[53px] w-[150px] rounded-tr rounded-br bg-cafranGray-semiDark text-base text-white lg:h-[50px] lg:w-[211px] lg:text-[18px] lg:font-medium">
+                                Alquilar
+                            </button>
+                        </div>
                     </div>
                     {/* Buscador */}
                     <Buscador />
