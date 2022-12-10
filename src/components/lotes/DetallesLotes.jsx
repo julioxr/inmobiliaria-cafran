d91560887548725eb6341d3d07f6351898be6f81
julio <julioroja987@gmail.com>
2022-12-10 16:52:56 -0300

    Finalizada la seccion Lotes responsiva y con sus estilos correspondientes

--- src/components/lotes/DetallesLotes.jsx
+++ src/components/lotes/DetallesLotes.jsx
@@ -4,9 +4,9 @@ const DetallesLotes = () => {
     return (
         <>
             {/*El width es 246 ajustar espaciado pero va por ahi*/}
-            <div className="z-10 order-3 -mt-14 h-48 max-w-[246px] rounded-md bg-white pt-4 pl-8 font-bold text-cafranGray-dark sm:max-w-[310px] md:col-span-4 md:-mt-[240px]  md:h-auto md:min-w-[345px] md:max-w-full md:rounded-none  md:bg-transparent md:px-12 lg:col-span-3 lg:-mt-[265px] lg:min-w-[470px] lg:px-[100px]">
+            <div className="z-10 order-3 -mt-14 h-48 max-w-[246px] rounded-md bg-white pt-4 pl-8 font-bold text-cafranGray-dark sm:max-w-[310px] md:col-span-4 md:-mt-[240px]  md:h-auto md:min-w-[320px] md:max-w-full md:rounded-none md:px-12 lg:col-span-3 lg:-mt-[265px] lg:min-w-[410px] lg:px-[100px]">
                 <h3 className="text-sm font-bold md:text-base">BARRIO</h3>
-                <h2 className="text-[28px] md:pb-2 md:text-[32px] lg:text-4xl">
+                <h2 className="text-[28px] md:pb-2 md:text-[32px] lg:w-[450px] lg:text-4xl">
                     La Candelaria
                 </h2>
                 <h3 className="pb-1 text-sm md:pb-3 lg:text-base">
